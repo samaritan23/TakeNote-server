efc4c00eb9c59b4470d22b008df40bef01ab8946
samaritan23 <bakshitamoghno6@gmail.com>
2022-05-15 01:58:49 +0530

    index.js updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is Take-A-Note server!!')
 })
 
 app.listen(port, () => {
